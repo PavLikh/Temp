fcdfd9465312203b6a81c12ba1f74eb84c039417
Pavel Likhopoy <plikhopoy@team.amocrm.com>
2021-10-01 15:05:32 +0300

    сделан  API подсчета суммы

--- src/App/Handler/HomePageHandler.php
+++ src/App/Handler/HomePageHandler.php
@@ -23,90 +23,32 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 class HomePageHandler implements RequestHandlerInterface
 {
     /** @var string */
-    private $containerName;
+    // private $containerName;
 
     /** @var Router\RouterInterface */
-    private $router;
+    // private $router;
 
     /** @var null|TemplateRendererInterface */
-    private $template;
+    // private $template;
 
-    public function __construct(
-        string $containerName,
-        Router\RouterInterface $router,
-        ?TemplateRendererInterface $template = null
-    ) {
-        $this->containerName = $containerName;
-        $this->router        = $router;
-        $this->template      = $template;
-    }
+    // public function __construct(
+    //     string $containerName,
+    //     Router\RouterInterface $router,
+    //     ?TemplateRendererInterface $template = null
+    // ) {
+    //     $this->containerName = $containerName;
+    //     $this->router        = $router;
+    //     $this->template      = $template;
+    // }
 
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
-        $data = [];
 
-        switch ($this->containerName) {
-            case Container::class:
-                $data['containerName'] = 'Aura.Di';
-                $data['containerDocs'] = 'http://auraphp.com/packages/4.x/Di/';
-                break;
-            case \Pimple\Psr11\Container::class:
-                $data['containerName'] = 'Pimple';
-                $data['containerDocs'] = 'https://pimple.symfony.com/';
-                break;
-            case ServiceManager::class:
-                $data['containerName'] = 'Laminas Servicemanager';
-                $data['containerDocs'] = 'https://docs.laminas.dev/laminas-servicemanager/';
-                break;
-            case InjectorContainer::class:
-                $data['containerName'] = 'Auryn';
-                $data['containerDocs'] = 'https://github.com/rdlowrey/Auryn';
-                break;
-            case ContainerBuilder::class:
-                $data['containerName'] = 'Symfony DI Container';
-                $data['containerDocs'] = 'https://symfony.com/doc/current/service_container.html';
-                break;
-            case 'Elie\PHPDI\Config\ContainerWrapper':
-            case \DI\Container::class:
-                $data['containerName'] = 'PHP-DI';
-                $data['containerDocs'] = 'http://php-di.org';
-                break;
-            case MinimalContainer::class:
-                $data['containerName'] = 'Chubbyphp Container';
-                $data['containerDocs'] = 'https://github.com/chubbyphp/chubbyphp-container';
-                break;
-        }
-
-        if ($this->router instanceof Router\AuraRouter) {
-            $data['routerName'] = 'Aura.Router';
-            $data['routerDocs'] = 'http://auraphp.com/packages/3.x/Router/';
-        } elseif ($this->router instanceof Router\FastRouteRouter) {
-            $data['routerName'] = 'FastRoute';
-            $data['routerDocs'] = 'https://github.com/nikic/FastRoute';
-        } elseif ($this->router instanceof Router\LaminasRouter) {
-            $data['routerName'] = 'Laminas Router';
-            $data['routerDocs'] = 'https://docs.laminas.dev/laminas-router/';
-        }
-
-        if ($this->template === null) {
-            return new JsonResponse([
-                'welcome' => 'Congratulations! You have installed the mezzio skeleton application.',
-                'docsUrl' => 'https://docs.mezzio.dev/mezzio/',
-            ] + $data);
-        }
-
-        if ($this->template instanceof PlatesRenderer) {
-            $data['templateName'] = 'Plates';
-            $data['templateDocs'] = 'http://platesphp.com/';
-        } elseif ($this->template instanceof TwigRenderer) {
-            $data['templateName'] = 'Twig';
-            $data['templateDocs'] = 'http://twig.sensiolabs.org/documentation';
-        } elseif ($this->template instanceof LaminasViewRenderer) {
-            $data['templateName'] = 'Laminas View';
-            $data['templateDocs'] = 'https://docs.laminas.dev/laminas-view/';
-        }
-
-        return new HtmlResponse($this->template->render('app::home-page', $data));
-
+        $target = $request->getQueryParams()['target'] ?? 'World';
+        $target = htmlspecialchars($target, ENT_HTML5, 'UTF-8');
+        return new HtmlResponse(sprintf(
+            '<h1>Hello %s</h1>',
+            $target
+        ));
     }
 }
