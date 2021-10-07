b66b003c6b77fb963d4af9bc3aa3c122e4aba1b7
Pavel Likhopoy <plikhopoy@team.amocrm.com>
2021-10-07 16:12:29 +0300

    удалены комментарии, лишние файлы

--- src/App/Handler/TokenHandler.php
+++ src/App/Handler/TokenHandler.php
@@ -15,35 +15,30 @@ use AmoCRM\Models\AccountModel;
 use League\OAuth2\Client\Token\AccessTokenInterface;
 
 
-
-use function time;
-
 class TokenHandler implements RequestHandlerInterface
 {
 
-
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
-
-        
+       
         $clientSecret = 't8iOfzbyqIoT2Ymlvarf3TfbILHaxk3ZDd3jaPDQiOXEex8SrSyIGWZ2rIXh29gE';
         $redirectUri = 'http://9809-80-250-213-62.ngrok.io/api/token';       
 
-        if ($_GET['client_id']){
-            $clientId = $_GET['client_id'];
+        if ($request->getQueryParams()['client_id']) {
+            $clientId = $request->getQueryParams()['client_id'];
         } else {
             $clientId = '49baced8-d07a-4e45-ab12-33849aa9b43a';
         }
 
         $apiClient = new \AmoCRM\Client\AmoCRMApiClient($clientId, $clientSecret, $redirectUri);
 
-        if (isset($_GET['referer'])) {
-            $apiClient->setAccountBaseDomain($_GET['referer']);
+        if (isset($request->getQueryParams()['referer'])) {
+            $apiClient->setAccountBaseDomain($request->getQueryParams()['referer']);
         }
 
 
 
-        if (!isset($_GET['code'])) {
+        if (!isset($request->getQueryParams()['code'])) {
             $state = bin2hex(random_bytes(16));
 
             $authorizationUrl = $apiClient->getOAuthClient()->getAuthorizeUrl([
@@ -54,16 +49,13 @@ class TokenHandler implements RequestHandlerInterface
             die;
         } 
 
-
-
-
 /**
  * Ловим обратный код
  */
 
         try {
 
-            $accessToken = $apiClient->getOAuthClient()->getAccessTokenByCode($_GET['code']);
+            $accessToken = $apiClient->getOAuthClient()->getAccessTokenByCode($request->getQueryParams()['code']);
 
              if (!$accessToken->hasExpired()) {
                 $data = [
@@ -80,29 +72,10 @@ class TokenHandler implements RequestHandlerInterface
              die((string)$e);
          }
 
-        // $apiClient->setAccessToken($accessToken);
-            // ->setAccountBaseDomain($accessToken->getValues()['baseDomain']);
-            // ->onAccessTokenRefresh(
-                // function (AccessTokenInterface $accessToken, string $baseDomain) {
-                    // saveToken(
-                    //     [
-                    //         'accessToken' => $accessToken->getToken(),
-                    //         'refreshToken' => $accessToken->getRefreshToken(),
-                    //         'expires' => $accessToken->getExpires(),
-                    //         'baseDomain' => $baseDomain,
-                    //     ]
-                    // );
-                // }
-        // );
         $ownerDetails = $apiClient->getOAuthClient()->getResourceOwner($accessToken);
 
-        //$account = $apiClient->account()->getCurrent(AccountModel::getAvailableWith());
+        $a = sprintf('Hello, %s!', $ownerDetails->getName());
 
-
-        // $a = sprintf('Hello, %s!', $ownerDetails->getName());
-
-        // return new JsonResponse($a);
-
-        return new HtmlResponse(sprintf('<h2>Hello, %s!<h2>', $ownerDetails->getName()));
+        return new JsonResponse($a);
     }
 }
